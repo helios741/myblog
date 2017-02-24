fe8847d78ebefeb6a0f49a502b0bb866639eca76
nannan <674876158@qq.com>
2017-02-24 17:52:30 +0800

    完成CSS的压缩

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@
 
 var gulp = require('gulp');
 var less = require("gulp-less");
+var minifyCSS = require('gulp-minify-css');
 gulp.task("style",function(){
     console.log("start");
     gulp.src("static/less/*.less")
         .pipe(less())
         .pipe(gulp.dest("static/css/"));
 });
+gulp.task("css",function(){
+    gulp.src("static/css/*.css")
+        .pipe(minifyCSS())
+        .pipe(gulp.dest("static/css/"));
+});
 gulp.task("watch",function(){
     gulp.watch("static/less/*.less",["style"]);
+    gulp.watch("static/css/*.css",["css"]);
 });
--- static/public/js/main.js
+++ static/public/js/main.js
@@ -1,6 +1,4 @@
 define(function(require,exports,module){
+    
     require("./header.js")();
-    /*var Pagination =  require("./pagination.js");
-    var pager = new  Pagination(1,25,5);
-    pager.render(".pagination");*/
-})
+});
