7ed95f6621b6ef0150cd45a677a4209783caf7f8
nannan <674876158@qq.com>
2017-07-22 19:36:02 +0800

    完成后端加载

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,6 @@
-const Koa = require('koa')
-const fs = require('fs')
-const app = new Koa()
-
-const Router = require('koa-router')
-
-let home = new Router()
-
-// 子路由1
-home.get('/', async ( ctx )=>{
-
-  ctx.body = "sdsds"
+require('babel-polyfill');
+require('babel-register')({
+    presets: ['es2015','stage-0'],
+    plugins: ['add-module-exports']
 })
-
-let router = new Router()
-router.use('/', home.routes(), home.allowedMethods())
-
-app.use(router.routes()).use(router.allowedMethods())
-
-app.listen(3002)
-console.log('[demo] route-use-middleware is starting at port 3000')
+require('./server');
