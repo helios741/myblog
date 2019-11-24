7ee559eae77c490268cd8ef6bde8d346a93949a1
helios741 <674876158@qq.com>
2019-11-24 13:27:32 +0800

    add all change for now

---
+++ learnpy/20190830_decorator/decorator.md
@@ -0,0 +1,8 @@
+
+## args 和 **kwargs是什么
+
+https://www.jianshu.com/p/61507f60fa29
+
+## 装饰器是什么
+
+装饰器就是切面的设计模式
--- src/2019/1001/test_range.py
+++
@@ -1,7 +0,0 @@
-for i in range(20):
-    print(i)
-
-print("---------")
-
-for i in range(0, 20):
-    print(i)
